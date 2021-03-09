3ca8e9a99b8b66a677b7617ca61b7786114f6512
vipulmantri28 <vipulmantri28@gmail.com>
2021-03-09 08:02:21 +0530

    scroll stop added

--- Services/banner2.js
+++ Services/banner2.js
@@ -48,41 +48,51 @@ fetch('https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=10',
     right.className += ' right-img';
 
     grandparent.appendChild(bannerparent);
-    
-    setInterval(function() {
-        let a = bannerparent.dataset.slidepos;
-        let bannerdiv = document.querySelectorAll('.banner-div')
 
-        if ( a == undefined || a == 0) {
-            a = 0;
-        }else {
-            a = Number(a); 
-        }
-        a -= 100
+    function scroller() {
+            this._interval = setInterval(function() {
+            let a = bannerparent.dataset.slidepos;
+            let bannerdiv = document.querySelectorAll('.banner-div')
 
-        if (a == -300) {
-            left.style.left = "335%"
-        }else if(a == -900) {
-            a = 0;
-            left.style.left = "0";
-        }else if (a == 0) {
-            right.style.right = "300"
-        }
-
-        bannerparent.dataset.slidepos = a;
-    
-        for (let i = 0; i < bannerdiv.length; i++) {
-
-            bannerdiv[i].style.transform = "translateX("+a+"%)";
-
-            if (a == 0) {
-                bannerdiv[i].style.transition = "";
+            if ( a == undefined || a == 0) {
+                a = 0;
             }else {
-                bannerdiv[i].style.transition = "transform 0.5s ease-in-out";
+                a = Number(a); 
             }
-        }
-    },3000)
+            a -= 100
+
+            if (a == -300) {
+                left.style.left = "335%"
+                right.style.right = "0"
+            }else if(a == -1000) {
+                a = 0;
+                left.style.left = "0";
+                right.style.right = "335%"
+            }
+
+            bannerparent.dataset.slidepos = a;
+        
+            for (let i = 0; i < bannerdiv.length; i++) {
+
+                bannerdiv[i].style.transform = "translateX("+a+"%)";
+
+                if (a == 0) {
+                    bannerdiv[i].style.transition = "";
+                }else {
+                    bannerdiv[i].style.transition = "transform 0.5s ease-in-out";
+                }
+            }
+        },3000)
+    }
+    bannerparent.addEventListener("mouseenter", function() {
+        clearInterval(_interval);
+    });
     
+    window.addEventListener('load', scroller());
+
+    bannerparent.addEventListener("mouseleave", function() {
+        scroller();
+    });
+
     console.log(data)
-    // document.addEventListener("DOMContentLoaded", scroller())
 }))
